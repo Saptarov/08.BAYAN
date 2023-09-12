da02f82ee58afd39f48ebbd97281cd50ea9554bf
archelo <archelobk@gmail.com>
2023-09-12 22:24:30 +0500

    fixed by review

--- src/main.cpp
+++ src/main.cpp
@@ -39,49 +39,51 @@ bool isDuplicate(const std::string& path, const std::vector<std::string>& hashes
     return true;
 }
 
-void scanDirectory(const std::string& path, const std::vector<std::string>& excludeDirs, const std::vector<std::string>& allowedExtensions, std::vector<std::string>& hashes, std::vector<std::vector<std::string>>& duplicates) {
+void scanDirectory(size_t blockSize, const std::string& path, const std::set<std::string>& excludeDirs, const std::vector<std::string>& allowedExtensions, std::vector<std::string>& hashes, std::vector<std::vector<std::string>>& duplicates) {
     std::vector<std::string> subdirs;
-    for (const directory_entry& entry : directory_iterator(path)) {
-        if (is_directory(entry.path()) && std::find(excludeDirs.begin(), excludeDirs.end(), entry.path().string()) == excludeDirs.end()) {
-            subdirs.push_back(entry.path().string());
-        } else if (is_regular_file(entry.path())) {
-            std::string fileExtension = boost::algorithm::to_lower_copy(entry.path().extension().string());
-            if (std::find(allowedExtensions.begin(), allowedExtensions.end(), fileExtension) != allowedExtensions.end()) {
-                std::string blockHash = hashBlock<boost::uuids::detail::sha1>(entry.path().string());
+    recursive_directory_iterator dir(path), end;
+    while (dir != end) {
+        auto iter = excludeDirs.find(dir->path().string());
+        if (is_directory(dir->path()) && iter == excludeDirs.end()) {
+            subdirs.push_back(dir->path().string());
+        } else if (is_regular_file(dir->path())) {
+            std::string fileExtension = boost::algorithm::to_lower_copy(dir->path().extension().string());
+            if (std::find(allowedExtensions.begin(), allowedExtensions.end(), fileExtension) != allowedExtensions.end() && file_size(dir->path().string()) <= blockSize) {
+                std::string blockHash = hashBlock<boost::uuids::detail::sha1>(dir->path().string());
                 if (std::find(hashes.begin(), hashes.end(), blockHash) != hashes.end()) {
                     hashes.push_back(blockHash);
-                    duplicates.back().push_back(entry.path().string());
+                    duplicates.back().push_back(dir->path().string());
                 } else {
                     hashes.push_back(blockHash);
-                    duplicates.push_back({ entry.path().string() });
+                    duplicates.push_back({ dir->path().string() });
                 }
             }
         }
     }
 
     for (const std::string& subdir : subdirs) {
-        scanDirectory(subdir, excludeDirs, allowedExtensions, hashes, duplicates);
+        scanDirectory(blockSize, subdir, excludeDirs, allowedExtensions, hashes, duplicates);
     }
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " directory [exclude_directory]..." << std::endl;
+        std::cerr << "Usage: " << argv[0] << " blocksize directory [exclude_directory]..." << std::endl;
         return 1;
     }
 
-    std::vector<std::string> excludeDirs;
+    std::set<std::string> excludeDirs;
     std::vector<std::string> allowedExtensions = { ".txt" };
-
-    for (int i = 2; i < argc; ++i) {
-        excludeDirs.push_back(argv[i]);
+    size_t blockSize = atoi(argv[2]);
+    for (int i = 3; i < argc; ++i) {
+        excludeDirs.insert(argv[i]);
       }
 
       std::string basePath = argv[1];
       std::vector<std::string> hashes;
       std::vector<std::vector<std::string>> duplicates;
 
-      scanDirectory(basePath, excludeDirs, allowedExtensions, hashes, duplicates);
+      scanDirectory(blockSize, basePath, excludeDirs, allowedExtensions, hashes, duplicates);
 
       for (const std::vector<std::string>& group : duplicates) {
           for (const std::string& path : group) {
